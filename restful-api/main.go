baaf5f2cb1e468c01cc8cf0415943e5e832e9555
yeguacelestial <yeguacelestial@gmail.com>
2021-05-11 16:15:21 -0500

    Create basic CRUD REST API

--- restful-api/main.go
+++ restful-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -32,7 +33,13 @@ func handleRequests() {
 
 	// replace http.HandleFunc with myRouter.HandleFunc
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+
+	// NOTE: Ordering is important.
+	// This has to be defined before the other '/article' endpoint
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
 	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 
 	// finally, instead of passing in nil, we want
@@ -49,6 +56,20 @@ func main() {
 	handleRequests()
 }
 
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	// unmarshal this into a new Article struct
+	var article Article
+	json.Unmarshal(reqBody, &article)
+
+	// Update our global articles array to include our new Article
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: returnAllArticles")
 	json.NewEncoder(w).Encode(Articles)
@@ -58,13 +79,52 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 
-	// fmt.Fprintf(w, "Key: "+key)
-
 	// Loop over all of our articles
-	// if the article.Id equals the key we pass in
-	// return the article encoded as JSON
 	for _, article := range Articles {
+
+		// if the article.Id equals the key we pass in
 		if article.Id == key {
+			// return the article encoded as JSON
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+
+	// we will need to extract the id of the article we wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our articles
+		if article.Id == id {
+			// updates our Articles array to remove the article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	// Parse the path parameters
+	vars := mux.Vars(r)
+
+	// Extract the id of the article we wish to update
+	id := vars["id"]
+
+	var updatedEvent Article
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	// Loop through all the articles
+	for index, article := range Articles {
+		if article.Id == id {
+			article.Title = updatedEvent.Title
+			article.Desc = updatedEvent.Desc
+			article.Content = updatedEvent.Content
+			Articles[index] = article
 			json.NewEncoder(w).Encode(article)
 		}
 	}
