3ffaa9a0ed4a7366144a5bf48f37359fbce32513
yeguacelestial <yeguacelestial@gmail.com>
2021-05-12 17:37:45 -0500

    Create unit tests

---
+++ unit-testing/testProject/main.go
@@ -0,0 +1,13 @@
+package main
+
+func Add(value1 int, value2 int) int {
+	return (value1 + value2)
+}
+
+func Substract(value1 int, value2 int) int {
+	return (value1 - value2)
+}
+
+func main() {
+
+}
