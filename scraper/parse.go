882162d7f3e17fc898e7e965400625a8807c19bb
yeguacelestial <yeguacelestial@gmail.com>
2021-05-13 11:37:47 -0500

    Parse the href from an anchor tag

--- scraper/parse.go
+++ scraper/parse.go
@@ -1,7 +1,13 @@
+/* STEPS
+1. Find <a> nodes in the document
+2. For each link node...
+	2.a build a Link
+3. Return the links
+*/
+
 package link
 
 import (
-	"fmt"
 	"io"
 
 	"golang.org/x/net/html"
@@ -22,20 +28,41 @@ func Parse(r io.Reader) ([]Link, error) {
 		return nil, err
 	}
 
-	dfs(doc, "")
-	return nil, nil
+	nodes := linkNodes(doc)
+
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+
+	return links, nil
 }
 
-func dfs(n *html.Node, padding string) {
-	msg := n.Data
+func buildLink(n *html.Node) Link {
+	var ret Link
 
-	if n.Type == html.ElementNode {
-		msg = "<" + msg + ">"
+	// Iterate through all the attributes of the node
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
 	}
 
-	fmt.Println(padding, msg) // padding is for indenting children elements
+	ret.Text = "TODO: Parse the text..."
+	return ret
+}
 
+func linkNodes(n *html.Node) []*html.Node {
+	// Verifiy that the node is a valid anchor tag
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+
+	// If the node is not anchor tag, still verifiy the child elements
+	var ret []*html.Node
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding+"  ")
+		ret = append(ret, linkNodes(c)...)
 	}
+	return ret
 }
