9547eae13aba61162429de5c1d0756f105cbb7f9
Yegua Celestial <yeguacelestial@gmail.com>
2021-05-31 16:11:54 -0500

    Create a basic REST API with Fiber

---
+++ fiber-rest-api/go.mod
@@ -0,0 +1,10 @@
+module github.com/yeguacelestial/go-playground/fiber-rest-api
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/jinzhu/gorm v1.9.16
+	github.com/klauspost/compress v1.11.13 // indirect
+	github.com/valyala/fasthttp v1.23.0 // indirect
+)
---
+++ fiber-rest-api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+	"github.com/yeguacelestial/go-playground/fiber-rest-api/book"
+	"github.com/yeguacelestial/go-playground/fiber-rest-api/database"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/books", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Database connection successfully opened.")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+
+	initDatabase()
+	defer database.DBConn.Close()
+
+	setupRoutes(app)
+
+	app.Listen(3000)
+}
